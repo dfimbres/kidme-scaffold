543d396c8f0e27b6576d2339e0a1268e37c426ee
David <dfimbres@users.noreply.github.com>
2022-04-19 18:52:43 -0700

    setup

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,7 +1,6 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol"; 
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
@@ -15,9 +14,26 @@ contract YourContract {
     // what should we do on deploy?
   }
 
+  function fib(uint8 n) public view returns(uint32) {
+      if(n < 2) {
+        // console.log(0);
+        // console.log(1);
+      }
+
+      uint32 curFib = 0;
+      uint32 oneFibAgo = 1;
+      uint32 twoFibAgo = 0;
+      for(uint i = 2; i < n; i++) {
+        curFib = oneFibAgo + twoFibAgo;
+        twoFibAgo = oneFibAgo;
+        oneFibAgo = curFib;
+      }
+
+      return curFib;
+  }
   function setPurpose(string memory newPurpose) public {
       purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
+      // console.log(msg.sender,"set purpose to",purpose);
       emit SetPurpose(msg.sender, purpose);
   }
 
